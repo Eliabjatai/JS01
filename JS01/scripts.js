1491f7f50943a9a8167b47cf26221e2c21531ccb
Eliab Jataí <eliabjatai69@gmail.com>
2025-02-04 21:53:04 -0300

    Add files via upload

---
+++ JS01/scripts.js
@@ -0,0 +1,48 @@
+// let idade = Number(prompt('Digite a Idade'))
+// let totalDias = idade * 365
+// alert(`total de dias = ${totalDias}`)
+
+// let horas = Number(prompt('Digite a hora atual'))
+// let totalMinutos = horas * 60
+// alert(`total de minutos = ${totalMinutos}`)
+
+// let real = Number(prompt('Digite um valor em real'))
+// let totalDolar = real * 5.87
+// alert(`total em dolar = ${totalDolar}`)
+
+// let distancia = Number(prompt('Digite a distancia percorrida'))
+// let consumo = Number(prompt('Digite o consumo total'))
+// let totalLitros = distancia / consumo
+// alert(`total de litros gastos = ${totalLitros}`)
+
+// let consumo = Number(prompt('Digite a quantidade de Litros gastos'))
+// let valorTotal = consumo * 6.15
+// alert(`valor total = ${valorTotal}`)
+
+// let nota1 = Number(prompt('Digite a nota 1'))
+// let nota2 = Number(prompt('Digite a nota 2'))
+// let nota3 = Number(prompt('Digite a nota 3'))
+// let media = (nota1 + nota2 + nota3) / 3
+// alert(`media total = ${media}`)
+
+
+// let peso = Number(prompt('Digite o seu peso'))
+// let altura = Number(prompt('Digte sua altura'))
+// let imc = peso / altura ** 2
+// alert(`total imc = ${imc}`)
+
+
+// let compra = Number(prompt('Digite o valor da compra'))
+// // let valorPago = Number(prompt('Digite o valor pago'))
+// // let troco = compra - valorPago
+// // alert(`valor do troco = ${troco}`)
+
+
+// let altura = Number(prompt('Digite sua altura em metros'))
+// let cm = altura * 100
+// alert(`altura em centimetros = ${cm}`)
+
+
+// let distanciaKm = Number(prompt('Digite a distancia em km'))
+// let totalEmm = distanciaKm * 1000
+// alert(`distancia total em metros = ${totalEmm}`)
